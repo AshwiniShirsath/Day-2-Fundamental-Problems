153a26ad073b56da9ff162839b82be832397aed3
AshwiniShirsath <shirsathashwini9495@gmail.com>
2022-04-11 21:13:06 +0530

    [ADDED] : Using While Loop Concept

---
+++ src/com/bridgelabz/WhileLoop/SumNaturalNumber.java
@@ -0,0 +1,24 @@
+package com.WhileLoop;
+/*
+ *  @author : Ashwini
+ *  Program for the sum of n natural numbers eg. Input N=5 output 1+2+3+4+5
+ */
+public class SumNaturalNumber {
+
+	public static void main(String[] args) {
+
+		int n = 5, i = 1, sum = 0;  
+		//executes until the condition returns true  
+		while(i <= n)  
+		{  
+		//adding the value of i into sum variable  
+		sum = sum + i;  
+		//increments the value of i by 1  
+		i++;  
+		}  
+		//prints the sum   
+		System.out.println("Sum of First 5 Natural Numbers is = " + sum);    
+
+	}
+
+}
